5abec0a7335b3968374647139e5d0e84fcfc927b
Yury Kavaliou <yury_kavaliou@epam.com>
2015-09-24 11:01:48 +0300

    Some changes

--- files/init_template.sh
+++ files/init_template.sh
@@ -3,5 +3,7 @@
 consul-template -consul ${CONSUL_ADDR:-consul}:${CONSUL_PORT:-8500} \
     -template /usr/local/bin/update_vip.ctmpl:/usr/local/bin/update_vip.sh -once
 
+/usr/local/bin/update_vip.sh
+
 consul-template -consul ${CONSUL_ADDR:-consul}:${CONSUL_PORT:-8500} \
     -template /usr/local/bin/update_vip.ctmpl:/usr/local/bin/update_vip.sh:/usr/local/bin/update_vip.sh
--- files/update_vip.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-ip a d 192.168.99.180 dev lo
-ip a a 192.168.99.181 dev lo
